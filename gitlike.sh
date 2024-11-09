d833ccce82ff0d366a68bc8b866490130b8e210b
bbhupen <bbhupen601@gmail.com>
2024-11-09 19:32:00 +0530

    add feature status, add (half done)

--- gitlike.sh
+++ gitlike.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
 
 # Function for initializing the gitlike config file
 function gitlike_init(){
@@ -30,14 +34,45 @@ function gitlike_init(){
 }
 
 # Function for checking the file diff
-function check_diff(){
-    diff -r . .gitlike/commits/$1
+check_diff() {
+    # Run diff and capture the output
+    diffs=$(diff -r . ".gitlike/commits/$1")
+
+    # Declare an associative array to store files by directory
+    declare -A files_by_dir
+
+    # Process each line of the diff output
+    while read -r line; do
+        # Check if the line indicates files only in one of the directories
+        if [[ "$line" =~ ^Only\ in ]]; then
+            # Extract directory and file name
+            directory=$(echo "$line" | awk '{print $3}' | sed 's/:$//')
+            file=$(echo "$line" | awk '{print $4}')
+            
+            # Append the file to the directory's file list
+            files_by_dir["$directory"]+="$file "
+        fi
+    done <<< "$diffs"
+
+    # Output the files grouped by directory
+    for dir in "${!files_by_dir[@]}"; do
+        # echo "Directory: $dir"
+        if [ "$2" == "add" ]; then
+            if [ "$dir" == "." ]; then
+                echo ${files_by_dir[$dir]} | sed "s/.gitlike //g"
+                break
+
+                fi
+        fi
+        echo -e "Untracked Files: ${RED}${files_by_dir[$dir]}${ENDCOLOR}"
+        echo
+    done
 }
 
 
 
 # Argument validation check
-if [ "$#" -ne 1 ]; then
+if [ "$#" -lt 1 ]; then
     echo "Usage: $0 <arugment 1>"
     echo "init"
     echo "status"
@@ -47,15 +82,34 @@ fi
 
 
 # The main code
+head_hash=$(cat .gitlike/HEAD)
 argument1=$1
 
 if [ "$argument1" == "init" ]; then
     gitlike_init
 elif [ "$argument1" == "status" ]; then
-    head_hash=$(cat .gitlike/HEAD)
     diff_res=$(check_diff $head_hash)
     echo ""
     echo $diff_res
+
+elif [ "$argument1" == "add" ]; then
+    if [ "$#" -ne 2 ]; then
+        echo "Usage: $0 add <argument>"
+        printf "\t%s\t%s\n" "." " add all files"
+        printf "\t%s\t%s\n" "file_name" " add specific file to a commit"
+        exit 1  
+    
+    fi
+
+
+    argument2=$2
+    if [ "$argument2" == "." ]; then
+        diff_res=$(check_diff $head_hash "add")
+        echo ""
+        echo -e "Files added to track: ${GREEN}$diff_res${ENDCOLOR}"
+        exit 1
+    fi
+
 else
     echo "Usage: $0 <arugment 1>"
     echo "init"
